d550ccb9287886e14e1b435bcdd6c7ea53fb093d
Funhuninja Studio <funhuninja@gmail.com>
2022-07-16 19:04:54 +0300

    v1.1.0 : Framebuffer added, OpenGL version 4.3 -> 4.5 ; ASSERT macro created.

--- src/Graphics/FrameBuffer.h
+++ src/Graphics/FrameBuffer.h
@@ -1,2 +1,31 @@
 #pragma once
-//TODO: napravi framebuffer class
+
+#pragma once
+
+namespace az
+{
+	struct FrameBufferSpecification
+	{
+		unsigned int Width, Height;
+		unsigned int Samples = 1;
+		bool SwapChainTarget = false;
+	};
+	class Framebuffer
+	{
+	private:
+		unsigned int m_Framebuffer, m_ColorAttachment, m_DepthAttachment;
+		FrameBufferSpecification m_Specification;
+	public:
+		Framebuffer(const FrameBufferSpecification& spec);
+		~Framebuffer();
+
+		void Resize();
+
+		void Bind() const;
+		void Unbind() const;
+
+		inline unsigned int GetColorAttachment() const	{ return m_ColorAttachment; }
+
+
+	};
+}
